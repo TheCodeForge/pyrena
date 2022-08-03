c3afcf23f8ec2fb73dcff2a12a7e02ae2a47a5c8
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-08-03 14:20:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
 	name="pyrena",
-	version="1.0",
-	description="Python wrapper for Arena QMS API. No license granted.",
+	version="1.0.0",
+	description="Python wrapper for Arena QMS API.",
 	url="https://github.com/thecodeforge/pyrena",
 	py_modules=["pyrena"],
 	install_requires=[
