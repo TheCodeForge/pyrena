feb4aed0fdc6f0af2fa9dd430c2b166751fcf9b0
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-08-03 14:33:43 -0400

    fix

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
 	name="pyrena",
-	version="1.0.0",
+	version="1.0.2",
 	description="Python wrapper for Arena QMS API.",
 	url="https://github.com/thecodeforge/pyrena",
 	py_modules=["pyrena"],
 	install_requires=[
 		"mistletoe",
 		"requests"
-	]
+	],
+	packages=setuptools.find_packages()
 )
