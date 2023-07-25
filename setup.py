44df4824a40e11c54c914a16eb24f6fe88cd79c1
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2023-07-25 16:41:38 -0400

    Bump version: 1.1.21 → 1.1.22

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
 	name="pyrena",
-	version="1.1.21",
+	version="1.1.22",
 	description="Python wrapper for Arena QMS API.",
 	url="https://github.com/thecodeforge/pyrena",
 	py_modules=["pyrena"],
